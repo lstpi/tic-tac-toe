40eeb46439a2cb40ebf31d84984172571ad6cb34
Lachlan St Pierre <9064067+lstpi@users.noreply.github.com>
2022-10-05 21:17:42 +1000

    Added winner check

--- src/index.js
+++ src/index.js
@@ -30,6 +30,9 @@ class Board extends React.Component {
     // Create a copy of the current state of the board
     const squares = this.state.squares.slice();
 
+    // Check the square is empty
+    if (squares[i] !== null || this.calculateWinner(squares)) return; 
+
     // Check current state and assign
     squares[i] = this.state.xIsNext ? 'X' : 'O';
 
@@ -44,8 +47,34 @@ class Board extends React.Component {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
